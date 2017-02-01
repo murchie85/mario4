6936363094c602edd03099c284f9b02407b72761
Adam <murchie85@gmail.com>
2017-02-01 22:25:06 +0000

    first

--- core/src/com/mcmurchie/mariobros/Screens/PlayScreen.java
+++ core/src/com/mcmurchie/mariobros/Screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -23,14 +26,27 @@ public class PlayScreen implements Screen { //GENERATE ALL GDX METHODS
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+
     //steps, define orthograthic cam, viewport, construct these in playscreen, set in render and resize
     //view ports. Screen viewport doesn't scale so bigger screen can see more
     //fitviewport always maintains ration
     public PlayScreen(MarioBros game){
         this.game = game;
+        //create cam used to follow mario
         gamecam = new OrthographicCamera();
+        //create viewport to maintain aspect ration
         gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+        //create hud for game score etc
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth()/2, gamePort.getWorldHeight()/2, 0);
 
     }
     @Override
@@ -38,12 +54,26 @@ public class PlayScreen implements Screen { //GENERATE ALL GDX METHODS
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched())
+            gamecam.position.x += 100 * dt; // temp
+    };
+
+
+    // for updating gameworld
+    public void update(float dt){
+        handleInput(dt);
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
     @Override
-    public void render(float delta) {
+    public void render(float delta) {// passed into update
+        update(delta);//
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-
+        renderer.render();// should be after screen is cleared
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
